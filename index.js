56028179a22e97309f5db1e9d6883a70ed82a614
navitha_eng <pnavitha@yahoo.com>
2019-08-01 19:02:10 -0700

    second message

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Hey, How are you doing? How is day so far?")
+
+document.addEventListener("DOMContentLoaded",()=>{
+    var today = new Date();
+  //  document.write(today);
+let time = document.querySelector("#time")
+time.innerHTML = "Time Now: "+ today
+
+})
+
+
